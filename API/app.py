3bb375b68a730d8291c1a5faf23dfcebed75c5b1
Felix1144 <junior01feliz@gmail.com>
2024-05-26 11:41:09 -0600

    Modificaciones en la API para el buen funcionamiento

--- API/app.py
+++ API/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, Response, jsonify
 import rospy
 from sensor_msgs.msg import CompressedImage
 import threading
@@ -16,19 +16,28 @@ def index():
 def get_image():
     global last_image
     if last_image is not None:
+        app.logger.info("Serving image")
         return Response(last_image, mimetype='image/jpeg')
     else:
+        app.logger.warning("No image received yet")
         return jsonify({"error": "No image received"}), 404
 
 def callback(data):
     global last_image
+    app.logger.info("Received image with timestamp: %s",data.header.stamp)
     last_image = data.data
+    app.logger.info("Received  image data size %d bytes",len(last_image))
 
 def listener():
     rospy.init_node('web_image_listener', anonymous=True)
-    rospy.Subscriber('/camera/image/compressed', CompressedImage, callback)
+    rospy.Subscriber("/image_topic/compressed", CompressedImage, callback)
+    rospy.loginfo("ROS node initialized and subscriber created")
     rospy.spin()
 
 if __name__ == '__main__':
-    threading.Thread(target=listener).start()
-    app.run(host='0.0.0.0', port=5000)
+    app.logger.info("Starting Flask Server")
+    flask_thread = threading.Thread(target=lambda: app.run(host='0.0.0.0',port=5000))
+    flask_thread.start()
+    listener()
+    #threading.Thread(target=listener).start()
+    #app.run(host='0.0.0.0', port=5000)
